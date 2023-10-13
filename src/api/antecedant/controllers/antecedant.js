dcdac4030927fe9bfe0bfa5d5b82248972fc492d
Julio Guimatsa <88024010+guimatsa@users.noreply.github.com>
2023-10-13 16:33:10 +0200

    cp: anteccedant add

---
+++ src/api/antecedant/controllers/antecedant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * antecedant controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::antecedant.antecedant');
---
+++ src/api/antecedant/services/antecedant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * antecedant service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::antecedant.antecedant');
